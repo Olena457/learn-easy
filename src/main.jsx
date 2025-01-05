3c8d50485ae437e167ee2a35a433e428176cdf4b
Olena457 <olenademianenko42@gmail.com>
2025-01-05 06:52:43 +0200

    change vite config

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,24 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store.js';
-import { ThemeProvider } from './contextTheme/ThemeContext.jsx';
+import { ThemeProvider } from './components/ThemeProvider/ThemeProvider.jsx';
 import App from './App.jsx';
+import store from './redux/store.js';
 import './index.css';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+        v7_normalizeFormMethod: true,
+        v7_routerProviderHydration: true,
+      }}
+    >
       <Provider store={store}>
         <ThemeProvider>
           <App />
