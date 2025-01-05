e3198fa215b50633f0dc0b2a7722018f6863bcba
Olena457 <olenademianenko42@gmail.com>
2025-01-05 09:51:24 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import styles from './HomePage.module.css';
 import HeroSection from '../../components/HeroSection/HeroSection.jsx';
 import ThemedImage from '../../components/ThemedImage/ThemedImage.jsx';
-import FeaturesSection from './../../components/FeaturesSection/FeaturesSection.jsx';
+import FeaturesSection from '../../components/FeaturesSection/FeaturesSection.jsx';
 
 const HomePage = () => {
   return (
